79fb36f332826a1511bae19ba2a5ad40015de67c
par-987 <908pbatra@gmail.com>
2023-04-12 20:31:20 +0530

    subtraction of two numbers

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sub;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+sub=a-b;
+printf("subtraction of two numbers=%d",sub);
+return 0;
+}
