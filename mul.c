014580198655604da5b5d3142379d69b42d76346
par-987 <908pbatra@gmail.com>
2023-04-12 20:37:07 +0530

    multiplication of two numbers

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,mul;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+mul=a*b;
+printf("multiplication of two numbers=%d",mul);
+return 0;
+}
