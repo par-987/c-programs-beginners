666b836eb56000fe0b3f12fa15eb1ca1caf75955
par-987 <908pbatra@gmail.com>
2023-04-12 20:42:04 +0530

    division of two numbers

---
+++ div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,div;
+printf("enter the value of a:");
+scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+div=a/b;
+printf("division of two numbers=%d",div);
+return 0;
+}
